94a1cb2535bfec5956b14dee47a7198b1ba83c5d
saharaeka18 <164349517+saharaeka18@users.noreply.github.com>
2024-07-04 17:47:42 +0700

    cors commit

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const bodyParser = require('body-parser');
 const koneksi = require('./config/database');
 const multer = require('multer');
 const path = require('path');
-
 const app = express();
 const port = 5000;
+
+const cors = require('cors');
+app.use(cors())
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
