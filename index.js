762c2547d8283791c333a5b918067a8225b43f91
saharaeka18 <164349517+saharaeka18@users.noreply.github.com>
2024-07-11 19:56:39 +0700

    app.delete commit

--- index.js
+++ index.js
@@ -152,6 +152,35 @@ app.get('/api/movies/filter/:judul', (req, res) => {
     });
 });
 
+// delete data
+app.delete('/api/movies/:id', (req, res) => {
+    // buat query sql untuk mencari data dan hapus
+    const querySearch = 'SELECT * FROM movies WHERE id = ?';
+    const queryDelete = 'DELETE FROM movies WHERE id = ?';
+
+    // jalankan query untuk melakukan pencarian data
+    koneksi.query(querySearch, req.params.id, (err, rows, field) => {
+        // error handling
+        if (err) {
+            return res.status(500).json({ message: 'Ada kesalahan', error: err });
+        }
+
+        // jika id yang dimasukkan sesuai dengan data yang ada di db
+        if (rows.length) {
+            // jalankan query delete
+            koneksi.query(queryDelete, req.params.id, (err, rows, field) => {
+                // error handling
+                if (err) {
+                    return res.status(500).json({ message: 'Ada kesalahan', error: err });
+                }
+                // jika delete berhasil
+                res.status(200).json({ success: true, message: 'Berhasil hapus data!' });
+            });
+        } else {
+            return res.status(404).json({ message: 'Data tidak ditemukan!', success: false });
+        }
+    });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
